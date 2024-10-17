e847e4c23441880611fbf52b8108714163731358
vasylblk <bulak.vasyl@lll.kpi>
2024-10-17 13:36:53 +0300

    hotfix task

--- func.cpp
+++ func.cpp
@@ -1,6 +1,11 @@
 #include "func.h"
 
 double Func::FuncA(int n) {
-    return 0;  
+    double sum = 0;
+    for (int i = 0; i < 3; ++i) {
+        sum += std::pow(1.0, i) / std::tgamma(i + 1);
+    }
+    return sum;
 }
+
 
